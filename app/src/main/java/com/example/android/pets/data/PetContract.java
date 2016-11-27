4745d85716e917887038857a5d3e5133ed9eb50d
zakafir <zak.afir@gmail.com>
2016-11-27 21:18:28 +0100

    Using SQLite format

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,38 +1,70 @@
 package com.example.android.pets.data;
 
-import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
- * Created by zakaria_afir on 10/09/2017.
+ * API Contract for the Pets app.
  */
+  public final class PetContract {
 
-public final class PetContract {
+       // To prevent someone from accidentally instantiating the contract class,
+       // give it an empty constructor.
+       private PetContract() {}
 
-    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_PETS = "pets";
+      /**
+    * Inner class that defines constant values for the pets database table.
+    * Each entry in the table represents a single pet.
+    */
+        public static final class PetEntry implements BaseColumns {
 
-    private PetContract() {
-    }
+          /**
+           * Name of database table for pets
+           */
+          public final static String TABLE_NAME = "pets";
 
-    public static final class PetEntry implements BaseColumns {
+          /**
+           * Unique ID number for the pet (only for use in the database table).
+           * <p/>
+           * Type: INTEGER
+           */
+          public final static String _ID = BaseColumns._ID;
 
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
-        //table
-        public static final String TABLE_NAME = "pets";
-        //Columns
-        public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_PET_NAME = "name";
-        public static final String COLUMN_PET_GENDER = "gender";
-        public static final String COLUMN_PET_BREED = "breed";
-        public static final String COLUMN_PET_WEIGHT = "weight";
-        //gender cases
-        public static final int GENDER_UNKNOWN = 0;
-        public static final int GENDER_FEMALE = 1;
-        public static final int GENDER_MALE = 2;
+          /**
+           * Name of the pet.
+           * <p/>
+           * Type: TEXT
+           */
+          public final static String COLUMN_PET_NAME = "name";
 
-        private PetEntry() {
-        }
-    }
+          /**
+           * Breed of the pet.
+           * <p/>
+           * Type: TEXT
+           */
+          public final static String COLUMN_PET_BREED = "breed";
+
+          /**
+           * Gender of the pet.
+           * <p/>
+           * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+           * or {@link #GENDER_FEMALE}.
+           * <p/>
+           * Type: INTEGER
+           */
+          public final static String COLUMN_PET_GENDER = "gender";
+
+          /**
+           * Weight of the pet.
+           * <p/>
+           * Type: INTEGER
+           */
+          public final static String COLUMN_PET_WEIGHT = "weight";
+
+          /**
+           * Possible values for the gender of the pet.
+           */
+          public static final int GENDER_UNKNOWN = 0;
+          public static final int GENDER_MALE = 1;
+          public static final int GENDER_FEMALE = 2;
+      }
 }
