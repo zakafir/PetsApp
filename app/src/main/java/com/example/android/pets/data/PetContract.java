2f69077f7396908575b069c4754aa6cc1616a28b
zakafir <zak.afir@gmail.com>
2017-09-10 16:34:25 +0100

    Creating the Contract

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,30 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by zakaria_afir on 10/09/2017.
+ */
+
+public final class PetContract {
+
+    public class PetEntry implements BaseColumns {
+
+        private PetEntry(){}
+
+        //table
+        public static final String TABLE_NAME = "pets";
+
+        //Columns
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        //gender cases
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+}
