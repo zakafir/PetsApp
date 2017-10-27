4f9f07ab7e3f8801f71a0c3fd6b32dc48f45ed0b
zakafir <zak.afir@gmail.com>
2017-10-27 15:16:56 +0100

    Exploring providers

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,123 +1,47 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
-import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
- * Created by zakaria_afir on 13/09/2017.
+ * Created by zakaria_afir on 27/10/2017.
  */
 
 public class PetProvider extends ContentProvider {
-
-    /**
-     * Tag for the log messages
-     */
-    public static final String LOG_TAG = PetProvider.class.getSimpleName();
-    //for our query method
-    private static final int PETS = 100;
-    private static final int PET_ID = 101;
-    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-
-    static {
-        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
-        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
-    }
-
-    public PetDbHelper mDbHelper;
-
-    /**
-     * Initialize the provider and the database helper object.
-     */
     @Override
     public boolean onCreate() {
-        mDbHelper = new PetDbHelper(this.getContext());
-        return true;
+        return false;
     }
 
-    /**
-     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
-     */
+    @Nullable
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
-
-        SQLiteDatabase database = mDbHelper.getReadableDatabase();
-        Cursor cursor;
-        int matcher = sUriMatcher.match(uri);
-        switch (matcher) {
-            case PETS:
-                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs,
-                        null, null, sortOrder);
-                break;
-
-            case PET_ID:
-                selection = PetContract.PetEntry._ID + "=?";
-                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
-                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection, selectionArgs,
-                        null, null, sortOrder);
-                break;
-            default:
-                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
-        }
-        return cursor;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
     }
 
-    /**
-     * Insert new data into the provider with the given ContentValues.
-     */
+    @Nullable
     @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
-        int matcher = sUriMatcher.match(uri);
-        Log.d(LOG_TAG, "Match between URI and URI matcher: " + matcher);
-        switch (matcher) {
-            case PETS:
-                return insertPet(uri, contentValues);
-            default:
-                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
-        }
+    public String getType(@NonNull Uri uri) {
+        return null;
     }
 
-    private Uri insertPet(Uri uri, ContentValues contentValues) {
-        SQLiteDatabase databaseRead = mDbHelper.getWritableDatabase();
-        long rowId = databaseRead.insert(PetContract.PetEntry.TABLE_NAME, null, contentValues);
-        return ContentUris.withAppendedId(uri, rowId);
-    }
-
-    /**
-     * Updates the data at the given selection and selection arguments, with the new ContentValues.
-     */
+    @Nullable
     @Override
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
     }
 
-    /**
-     * Delete the data at the given selection and selection arguments.
-     */
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        SQLiteDatabase database = mDbHelper.getReadableDatabase();
-        int matcher = sUriMatcher.match(uri);
-        switch (matcher) {
-            case PETS:
-                return database.delete(PetContract.PetEntry.TABLE_NAME, null, null);
-
-            default:
-                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
-        }
-    }
-
-    /**
-     * Returns the MIME type of data for the content URI.
-     */
-    @Override
-    public String getType(Uri uri) {
-        return null;
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
     }
 }
