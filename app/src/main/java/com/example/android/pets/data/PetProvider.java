b16598865c67f810690f442d98eb398fc74ad9b5
zakafir <zak.afir@gmail.com>
2017-11-03 15:13:24 +0000

    Content Provider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,26 +1,59 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by zakaria_afir on 27/10/2017.
  */
 
 public class PetProvider extends ContentProvider {
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+    PetDbHelper petDbHelper;
+
+    private static final int PETS = 1;
+    private static final int PET_ID = 2;
+    public static UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS,PETS);
+
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS+"/#",PET_ID);
+    }
     @Override
     public boolean onCreate() {
+        petDbHelper = new PetDbHelper(this.getContext());
         return false;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        SQLiteDatabase sqLiteDatabase = petDbHelper.getReadableDatabase();
+        Cursor cursor;
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case PETS :
+                cursor = sqLiteDatabase.query(PetContract.PetEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+                break;
+            case PET_ID :
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = sqLiteDatabase.query(PetContract.PetEntry.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("unknown URI on the query method " + uri);
+        }
+        return cursor;
     }
 
     @Nullable
@@ -32,7 +65,26 @@ public class PetProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        Uri myNewUri;
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case PETS :
+                myNewUri = insertPet(uri,values);
+                break;
+            default:
+                throw new IllegalArgumentException("problems on the insert method " + uri);
+        }
+        return myNewUri;
+    }
+
+    private Uri insertPet(Uri uri, ContentValues contentValues) {
+        SQLiteDatabase sqLiteDatabase = petDbHelper.getWritableDatabase();
+        long id = sqLiteDatabase.insert(PetContract.PetEntry.TABLE_NAME,null,contentValues);
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        return ContentUris.withAppendedId(uri,id);
     }
 
     @Override
